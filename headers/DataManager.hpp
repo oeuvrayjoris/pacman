a2df99d39cb75def8c950034ef4c0693da6122d9
Jordan Vilsaint <vilsaint.jordan@gmail.com>
2017-12-28 21:07:33 +0100

    Ajout lib : Rapidjson & Creation de la classe DataManager

---
+++ headers/DataManager.hpp
@@ -0,0 +1,14 @@
+//
+// Created by Jordan on 28/12/2017.
+//
+
+#ifndef IMACGL_DATAMANAGER_HPP
+#define IMACGL_DATAMANAGER_HPP
+
+
+class DataManager {
+
+};
+
+
+#endif //IMACGL_DATAMANAGER_HPP
---
+++ src/DataManager.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Jordan on 28/12/2017.
+//
+
+#include "../headers/DataManager.hpp"
