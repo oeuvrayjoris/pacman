ed7c91a667e7a8445e67df207f7428fbdc964230
oeuvrayjoris <oeuvray.joris@gmail.com>
2017-12-29 16:29:27 +0100

    Board.hpp updated

--- headers/Board.hpp
+++ headers/Board.hpp
@@ -5,17 +5,7 @@
 #include <vector>
 
 class Board {
-<<<<<<< HEAD
-    public:
-        // Getters
-		int getLargeur();
-		int getHauteur();
-		std::vector<std::vector<int>> getTab();
-=======
->>>>>>> 4085856f460a9d501551e89c9d99c7b8d41c41d6
-
 public:
-
 	// Constructors
 	Board();
 
@@ -25,24 +15,21 @@ public:
 	// Getters
 	int getM();
 	int getN();
-    std::vector<std::vector<int>> getTab();
+	std::vector<std::vector<int>> getTab();
 
 	// Setters
-	void setM(int a);
-	void setN(int b);
-	void setTab(std::vector<std::vector<int>> newTab);
+	void setM(int new_m);
+	void setN(int new_n);
+	void setTab(std::vector<std::vector<int>> new_tab);
 
-	private:
-        // Attr
-<<<<<<< HEAD
-		int largeur;
-		int hauteur;
-		int tab[25][25];
-=======
-		int m;
-		int n;
-        std::vector<std::vector<int>> tab;
->>>>>>> 4085856f460a9d501551e89c9d99c7b8d41c41d6
+	// Methods
+	void changeValue(int m, int n, int value);
+
+private:
+	// Attr
+	int m;
+	int n;
+	std::vector<std::vector<int>> tab;
 };
 
 #endif
